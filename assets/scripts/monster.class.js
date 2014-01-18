6922f9f812fcdc2e23bcb39c44b732cb83232cc0
Samuel Wagner <mukah@live.com>
2014-01-17 22:32:59 -0200

    testing point of light

--- assets/scripts/monster.class.js
+++ assets/scripts/monster.class.js
@@ -1,8 +1,9 @@
 // PLAYER
 function Monster(name, texture) {
 	Creature.apply(this, arguments);
-	
-	this.teste = function() {
+
+	setTimeout(seek, 1000);
+	this.seek = function() {
 		return 'method';
 	}
 }
