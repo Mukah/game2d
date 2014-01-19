a1fb08b43866628a1f836ae3c500a0307a9d4e6b
Samuel Wagner <mukah@live.com>
2014-01-19 02:23:43 -0200

    monster following

--- assets/scripts/monster.class.js
+++ assets/scripts/monster.class.js
@@ -4,16 +4,37 @@ function Monster(attrs) {
 
 	// Attributes
 	this.fov = attrs['fov'];
+	this.target = undefined;
+	this.folowing = false;
 
 	// Functions
 	var self = this;
 	this.seek = function() {
-		if(PIXI.Point.distance(self.position, Game.protagonist.position) <= self.fov) {
-			console.log("fdp");
+		if(self.target != undefined) {
+			if(self.folowing == false) {
+				self.follow();
+				self.folowing = true;
+			}
+		} else {
+			if(PIXI.Point.distance(self.position, Game.protagonist.position) <= self.fov) {
+				self.target = Game.protagonist;
+				console.log("fdp");
+			}
 		}
 	}
 
 	this.startSeek = function() {
 		setInterval(this.seek, 1000);
+	}
+	this.follow = function() {
+		var interval = setInterval(function() {
+			if(self.target != undefined) {
+				if(PIXI.Point.distance(self.position, Game.protagonist.position) > 1) {
+					self.walkTowards(self.target.position);
+				}
+			} else {
+				clearInterval(interval);
+			}
+		}, this.walkSpeed);
 	}
 }
